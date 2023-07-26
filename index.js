5d850fc8dfd5af2d7bae6fa9d6c483a913f488f4
GCT <samgalaxycard>
2023-07-26 15:54:34 +0530

    added more func

--- index.js
+++ index.js
@@ -44,20 +44,36 @@ app.use(bodyParser());
 
 
 // * Koa router
-// Getting all the users ---------- GET
+// Getting all the users ---------------- GET
 router.get('/users', async (ctx) => {
-    const posts = await User.findAll({ paranoid: false });
+    const posts = await User.findAll({ paranoid: false, attributes: { exclude: ['updatedAt', 'deletedAt', 'createdAt'] } });
     ctx.body = posts;
 });
 
-//Creating the user ----------- POST
+
+
+//Getting 1 user ------------------------ GET
+router.get('/users/:id', async (ctx) => {
+    const { id } = ctx.params;
+    const user = await User.findByPk(id, { attributes: { exclude: ['updatedAt', 'deletedAt', 'createdAt'] } });
+    if (!user) {
+        ctx.status = 404;
+        ctx.body = { error: 'User not found' }
+        return;
+    }
+    ctx.body = user;
+})
+
+
+//Creating the user --------------------- POST
 router.post('/users', async (ctx) => {
     const { name, gender, age } = ctx.request.body;
     const user = await User.create({ name, gender, age });
-    ctx.body = user
+    ctx.body = { name, gender, age }
 })
 
-//Updating the user ---------- PUT
+
+//Updating the user ---------------------- PUT
 router.put('/users/:id', async (ctx) => {
     const { id } = ctx.params;
     const { name, gender, age } = ctx.request.body;
@@ -68,10 +84,10 @@ router.put('/users/:id', async (ctx) => {
         return;
     }
     await user.update({ name, gender, age });
-    ctx.body = user;
+    ctx.body = { name, gender, age };
 })
 
-//Deleting the user ---------- DELETE
+//Deleting the user ---------------------- DELETE
 router.delete('/users/:id', async (ctx) => {
     const { id } = ctx.params;
     const user = await User.findByPk(id);
@@ -85,25 +101,22 @@ router.delete('/users/:id', async (ctx) => {
 })
 
 
-// //Hard
-// router.delete('/users/hard/:id', async (ctx) => {
-//     const { id } = ctx.params;
-//     const user = await User.findByPk(id);
-//     if (!user) {
-//         ctx.status = 404;
-//         ctx.body = { error: 'User not found' }
-//         return;
-//     }
-//     await user.destroy({ force: true });
-//     ctx.body = { message: "User hard deleted successfully" }
-// })
+//Hard delete the user -------------------- DELETE
+router.delete('/users/hard/:id', async (ctx) => {
+    const { id } = ctx.params;
+    const user = await User.findByPk(id);
+    if (!user) {
+        ctx.status = 404;
+        ctx.body = { error: 'User not found' }
+        return;
+    }
+    await user.destroy({ force: true });
+    ctx.body = { message: "User hard deleted successfully" }
+})
 
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-
-
 
 
 const PORT = 3000;
